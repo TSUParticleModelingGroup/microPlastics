bc8dd2acf392a214953948c9ae9382c0d9a399c7
MBane04 <96203961+MBane04@users.noreply.github.com>
2024-07-03 15:12:33 -0500

    Added changeable shake mag and fixed recenter

---
+++ setperms.sh
@@ -0,0 +1,9 @@
+#run the below line first (goves the file permision to be run)
+#chmod 755 setperms.sh
+chmod u+rwx ../src
+chmod 755 callBackFunctions.h
+chmod 755 microPlastics.cu
+chmod 755 compile.sh
+chmod 755 run.sh
+chmod 775 Stills
+chmod u+rw simulationSetup
